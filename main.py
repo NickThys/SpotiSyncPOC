9291055f16c8893f40050e78e4518f6ee8c32b84
nick <s114623@ap.be>
2022-06-09 22:28:47 +0200

    Auto stash before checking out "origin/master"

--- main.py
+++ main.py
@@ -16,6 +16,28 @@ class SaveSongs:
         self.tracksToAdd = []
         self.find_playlist_to_sync()
 
+    def sync_playlist(self, playlistIdToSync, playlistIdsToAdd):
+        nrOfSongs = 0
+        tracksToAdd = []
+        playlistToSyncTracks = self.get_playlist_tracks(playlistIdToSync)
+        print("------------------------------------")
+        print("Sync playlist: "+self.get_playlist_name(playlistIdToSync))
+        print("from playlist:")
+
+        for playlistId in playlistIdsToAdd:
+            print(self.get_playlist_name(playlistId))
+            tracks = self.get_playlist_tracks(playlistId)
+            for track in tracks:
+                if track not in playlistToSyncTracks:
+                    tracksToAdd.append(track)
+            nrOfSongs += len(tracks)
+        print("------------------------------------")
+        self.post_new_songs(tracksToAdd, playlistIdToSync)
+
+        print("------------------------------------")
+        print("nr of songs to added: " + str(len(tracksToAdd)))
+        print()
+
     def find_playlist_to_sync(self):
         nrOfSongs = 0
         for playlistId in self.playlistToAdd:
@@ -46,7 +68,43 @@ class SaveSongs:
 
         return tracks
 
-    def get_next_tracks(self, uri):
+    def post_new_songs(self, tracks, playlistId):
+        n = 100
+        trackChunks = [tracks[i:i + n] for i in range(0, len(tracks), n)]
+        for chunk in trackChunks:
+            tracklist = self.make_comma_separated_list(chunk)
+            response = self.post_songs(playlistId, tracklist)
+            while "error" in response:
+                songToDelete = response['error']['message'][19:]
+                print("song to add manually: " + songToDelete[16:])
+                song = []
+                strLen = 15
+                while len(song) != 1:
+                    strLen += 1
+                    song = [string for string in chunk if songToDelete[0:strLen] in string]
+                chunk.remove(song[0])
+                if len(chunk) != 0:
+                    tracklist = self.make_comma_separated_list(chunk)
+                    response = self.post_songs(playlistId, tracklist)
+                else:
+                    response.pop('error')
+
+    def post_songs(self, playlistId, tracklist):
+        response = requests.post(
+            "https://api.spotify.com/v1/playlists/{}/tracks?uris={}".format(playlistId, tracklist),
+            headers={"content-type": "application/json",
+                     "Authorization": "Bearer {}".format(
+                         self.spotify_token)})
+        return response.json()
+
+    @staticmethod
+    def make_comma_separated_list(tracks):
+        trackList = ""
+        for track in tracks:
+            trackList += track + ","
+        return trackList[:-1]
+
+    def get_request(self, uri):
         response = requests.get(uri, headers={"content-type": "application/json",
                                               "Authorization": "Bearer {}".format(
                                                   self.spotify_token)})
